da10273d309ae8f82e9abe9041813dde412e322c
BoolAid-Man <marcouxaustin@gmail.com>
2021-04-24 13:15:49 -0400

    Initial Project Setup

---
+++ app/src/main/java/com/marcoux/austinmarcoux1099028final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.marcoux.austinmarcoux1099028final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
